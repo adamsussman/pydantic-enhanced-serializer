1c374bbd4db76a29866d97856e49fee6f991cf0d
Adam Sussman <adam@homeboost.com>
2023-02-27 22:19:46 -0800

    renderer replacement style django integration

---
+++ pydantic_sparsefields/integrations/django_ninja.py
@@ -0,0 +1,41 @@
+from asgiref.sync import async_to_sync
+from django.http import HttpRequest
+from django_ninja.parsers import Parser
+from django_ninja.renderes import JSONRenderer
+from pydantic import BaseModel
+
+
+class PydanticFieldSetsRenderer(JSONRenderer):
+    def __init__(cls, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields_parameter_name = kwargs.get("fields_parameter_name", "fields")
+
+
+    def pydantic_to_dict(
+        self,
+        data: BaseModel,
+        request: HttpRequest,
+        *,
+        by_alias: bool = False,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+    ) -> dict:
+        # not very efficent to have to reparse the request...
+        parser = Parser()
+        fieldsets = (
+            parser.parse_body(request).get(self.fields_parameter_name) or
+            parser.parse_querydict(request.GET).get(self.fields_parameter_name) or
+            []
+        )
+
+        result_data = async_to_sync(render_fieldset_model)(
+            model=data,
+            fieldsets=fieldsets,
+            maximum_expansion_depth=5,
+            raise_error_on_expansion_not_found=False,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+        )
+        return result_data
